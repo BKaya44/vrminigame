bf1088e9afd0bf8f43925d7468345f116b5ac7a0
BKaya44 <11460681+BKaya44@users.noreply.github.com>
2021-01-22 16:05:50 +0100

    Add game files

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSettings : MonoBehaviour
+{
+    
+    [SerializeField] private AudioSource music;
+    [SerializeField] private Toggle musicToggle;
+    [SerializeField] private Slider musicSlider;
+
+    void start(){
+        musicSlider.maxValue = 1; 
+        musicSlider.minValue = 0;
+    }
+    void Update() {
+        if (PlayerPrefs.HasKey("music")) {
+            if(musicToggle.isOn){
+                PlayerPrefs.SetInt("music", 1);
+                musicToggle.isOn = true;
+                music.enabled = true;
+            } else {
+                PlayerPrefs.SetInt("music", 0);
+                musicToggle.isOn = false;
+                music.enabled = false;
+            }
+        }
+    }
+
+    void OnEnable() {
+        musicSlider.onValueChanged.AddListener(delegate { changeVolume(musicSlider.value); });
+    }
+
+    public void changeVolume(float volume){
+        music.volume = volume;
+        PlayerPrefs.SetFloat("musicVolume", volume);
+        PlayerPrefs.Save();
+    }
+    
+    public void Awake () {
+        //Check if player has music key and makes one if user doesn't have one.
+        if (!PlayerPrefs.HasKey("musicVolume")) {
+            PlayerPrefs.SetFloat("musicVolume", 0);
+            PlayerPrefs.Save();
+        } else {
+            music.volume = PlayerPrefs.GetFloat("musicVolume");
+            musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        }
+        if (!PlayerPrefs.HasKey("music")) {
+            PlayerPrefs.SetInt("music", 1);
+            musicToggle.isOn = true;
+            music.enabled = true;
+            PlayerPrefs.Save();
+        } else {
+            //Check if music is turned on or not.
+            if (PlayerPrefs.GetInt ("music") == 0) {
+                music.enabled = false;
+                musicToggle.isOn = false;
+            } else {
+                music.enabled = true;
+                musicToggle.isOn = true;
+            }
+        }
+  }
+
+}
---
+++ Assets/Scripts/RingtossSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingtossSettings : MonoBehaviour
+{
+    [SerializeField] GameObject ring;
+    [SerializeField] Rigidbody ringBody;
+    [SerializeField] private Vector3 respawnPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter (Collision c){
+        //basketballAudio.PlayOneShot(RandomClip());
+        if (c.gameObject.tag == "RingScoreCone"){
+            ring.transform.position = respawnPosition;
+            ring.transform.rotation = new Quaternion(0,0,0,0);
+            ringBody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
